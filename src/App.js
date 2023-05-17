77d2a0936f25d456039903f8740afd9273418cca
StacyGru <stacy.gru@gmail.com>
2023-05-17 12:25:52 +0300

    Display catalog items for all categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./index.css"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import MainPage from "./pages/MainPage"
 import AboutUs from "./pages/AboutUs"
 import Contacts from "./pages/Contacts";
@@ -14,29 +14,35 @@ import Registration from "./pages/Registration";
 import Basket from "./pages/Basket";
 import Catalog from "./pages/Catalog";
 import CatalogComputers from "./pages/CatalogComputers";
-import CatalogList from "./pages/CatalogList";
+import SystemUnitsList from "./pages/SystemUnitsList";
+import ComputerKitsList from "./pages/ComputerKitsList";
+import MonitorsList from "./pages/MonitorsList";
+import SpecialOffersList from "./pages/SpecialOffersList";
 
 function App() {
     return (
         <div className="grid-layout text-xs lg:text-sm 2xl:text-lg font-normal text-darkBlue bg-grayWhite">
-            <Header/>
-            <Routes>
-                <Route path="/" element={<MainPage/>} />
-                <Route path="/about_us" element={<AboutUs/>} />
-                <Route path="/contacts" element={<Contacts/>} />
-                <Route path="/payment" element={<Payment/>} />
-                <Route path="/delivery" element={<Delivery/>} />
-                <Route path="/guarantees" element={<Guarantees/>} />
-                <Route path="/for_wholesalers" element={<ForWholesalers/>} />
-                <Route path="/login" element={<LogIn/>} />
-                <Route path="/registration" element={<Registration/>} />
-                <Route path="/basket" element={<Basket/>} />
-                <Route path="/catalog" element={<Catalog/>} />
-                <Route path="/catalog" element={<Catalog/>} />
-                <Route path="/computers" element={<CatalogComputers/>} />
-                <Route path="/system_units" element={<CatalogList/>} />
-            </Routes>
-            <Footer/>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<MainPage/>} />
+                    <Route path="/about_us" element={<AboutUs/>} />
+                    <Route path="/contacts" element={<Contacts/>} />
+                    <Route path="/payment" element={<Payment/>} />
+                    <Route path="/delivery" element={<Delivery/>} />
+                    <Route path="/guarantees" element={<Guarantees/>} />
+                    <Route path="/for_wholesalers" element={<ForWholesalers/>} />
+                    <Route path="/login" element={<LogIn/>} />
+                    <Route path="/registration" element={<Registration/>} />
+                    <Route path="/basket" element={<Basket/>} />
+                    <Route path="/catalog" element={<Catalog/>} />
+                    <Route path="/catalog" element={<Catalog/>} />
+                    <Route path="/computers" element={<CatalogComputers/>} />
+                    <Route path="/system_units" element={<SystemUnitsList/>} />
+                    <Route path="/computer_kits" element={<ComputerKitsList/>} />
+                    <Route path="/monitors" element={<MonitorsList/>} />
+                    <Route path="/special_offers" element={<SpecialOffersList/>} />
+                </Routes>
+                <Footer/>
         </div>
     );
 }
---
+++ src/pages/MonitorsList.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import React from "react"
+import CatalogItemImg from "../media/catalog_item.png";
+import Basket from "../media/basket.png";
+import {Link} from "react-router-dom";
+
+class MonitorsList extends React.Component {
+
+    state = { details: [], }
+
+    componentDidMount() {
+        let data;
+        axios.get('http://127.0.0.1:8000/monitors')
+            .then(res => {
+                data = res.data;
+                this.setState({
+                    details: data
+                });
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="grid grid-cols-3 grid-rows-1 px-1/10">
+                    <p className="h-fit w-fit text-xs font-light text-mainGray">
+                        <Link to="/catalog" className="hover:underline">Каталог товаров</Link>
+                        <span> > </span>
+                        <Link to="/monitors" className="hover:underline">Мониторы</Link>
+                    </p>
+                    <h1 className="text-xl lg:text-3xl font-bold mb-10 text-center">Мониторы</h1>
+                    <p className="text-mainGray justify-self-end">{this.state.details.length} товар</p>
+                    <div/>
+                </div>
+                <div className="flex flex-wrap justify-center gap-5">
+                    {this.state.details.map((product, id) => (
+                        <div className="w-72 bg-mainWhite py-5 px-10 drop-shadow-sm rounded-xl flex flex-col items-center">
+                            <img src={product.picture ? "http://localhost:8000"+product.picture : CatalogItemImg} className="object-contain h-64 m-5"/>
+                            <h2 className="text-xl mb-5">{product.name}</h2>
+                            <p className="font-light mb-5 grow">{product.short_description}</p>
+                            <div className="w-full flex justify-between items-center self-end">
+                                <h2 className="text-2xl">{product.price} ₽</h2>
+                                <button className="bg-mainOrange drop-shadow-sm rounded-xl h-12 w-12 flex justify-center items-center">
+                                    <img src={Basket} className="h-8"/>
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+export default MonitorsList;
